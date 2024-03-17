b929724dbeb038a2fc8e3113cd93cea8c83b021a
ElvisKiongo <elvis.kiongo@student.moringaschool.com>
2024-03-17 21:10:34 +0300

    Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+// Basic math functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+// Additional functions
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
